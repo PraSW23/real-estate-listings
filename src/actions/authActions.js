df79be76e5ca07e221737a827cbb7760d19aa452
Prakhar <saraswatprakhar09@gmail.com>
2024-06-11 00:23:41 +0530

    commit

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -75,9 +75,19 @@ export const register = user => async dispatch => {
     setAuthToken(res.data.token); 
     dispatch(loadUser()); 
   } catch (err) {
+    let errorMessage = 'An error occurred. Please try again later.';
+    if (err.response && err.response.status === 400) {
+      errorMessage = 'User already exists. Please use a different email.';
+    } else if (err.response && err.response.data && err.response.data.message) {
+      errorMessage = err.response.data.message;
+    }
     dispatch({
-      type: AUTH_ERROR
+      type: AUTH_ERROR,
+      payload: errorMessage
     });
+
+    // Throwing an error here when registration fails
+    throw new Error(errorMessage);
   }
 };
 
